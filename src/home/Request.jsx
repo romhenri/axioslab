95b6945ccd77b2ec9d62297e3625c0787f50b84f
Rômulo Henri <romulohenri000@gmail.com>
2024-04-20 16:24:23 -0300

    feat: Cancel Request

--- src/home/Request.jsx
+++ src/home/Request.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
 
+const source = axios.CancelToken.source();
+
 export const Request = () => {
   const [color, setColor] = React.useState("red");
 
@@ -12,13 +14,13 @@ export const Request = () => {
       <div
       className={`w-12 h-12 rounded-full ${
         color === "red" ? "bg-red-500" 
+        : color === "yellow" ? "bg-yellow-500"
+        : color === "orange" ? "bg-orange-500"
         : color === "green" ? "bg-green-500" 
-        : color === "yellow" ? "bg-yellow-500" 
         : "bg-red-500"
       }`}
       >
       </div>
-    
     
     <button
       className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded
@@ -26,15 +28,32 @@ export const Request = () => {
       "
       onClick={() => {
         setColor("yellow");
-        axios.get("https://hub.dummyapis.com/delay?seconds=3")
+        axios.get(
+          "https://hub.dummyapis.com/delay?seconds=3",
+          {cancelToken: source.token}
+        )
 
         .then((response) => {
           console.log(response);
           setColor("green");
         })
+        .catch(thrown => {
+          if (axios.isCancel(thrown)) {
+            setColor("orange");
+          } else {
+            // Handle other errors
+          }
+        });
       }}
     >
       Request
+    </button>
+    <button
+      onClick={() => {
+        source.cancel('Operation canceled by the user.');
+      }}
+    >
+      Cancel
     </button>
     </div>
   </section>
